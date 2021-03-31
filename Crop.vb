c10d2dc139f9005feb0dbb8b8d8cb5a890de2e17
Jeffrey Saelee <gwfds123@gmail.com>
2021-03-30 20:06:31 -0700

    Added Crop Class

---
+++ Crop.vb
@@ -0,0 +1,70 @@
+﻿Public Class Crop
+    Private strType As String
+    Public Property SoilMoisture As Double
+    Public Property Temperature As Double
+    Public Property Days As Integer
+    Public Property AvgHeight As Double
+    Public Property ReadyPlant As Boolean = False
+    Public Property ReadyHarvest As Boolean = False
+
+    Public Property Type As String
+        Get
+            Return strType
+        End Get
+        Set(value As String)
+            value = value.ToLower
+            If value = "corn" Then
+                strType = "Corn"
+            ElseIf value = "soybean" Then
+                strType = "Soybean"
+            ElseIf value = "wheat" Then
+                strType = "Wheat"
+            Else
+                strType = "Invalid"
+            End If
+        End Set
+    End Property
+
+    Public Sub New()
+        strType = "Invalid"
+        _SoilMoisture = 100
+        _Days = 0
+        _AvgHeight = 0
+    End Sub
+
+    Private Function Plant() As Boolean
+        If strType = "Corn" Then
+            If _SoilMoisture >= 25 And _SoilMoisture <= 50 Then
+                If _Temperature >= 60 And _Temperature <= 90 Then
+                    Return True
+                End If
+            End If
+        ElseIf strType = "SoyBean" Then
+            If _Temperature >= 60 Then
+                Return True
+            End If
+        ElseIf strType = "Wheat" Then
+            Return True
+        End If
+        Return False
+    End Function
+    Private Function Harvest() As Boolean
+        If strType = "Corn" Then
+            If Days >= 60 Or AvgHeight >= 8 * 12 Then
+                If AvgHeight >= 4 * 12 Then
+                    Return True
+                End If
+            End If
+        ElseIf strType = "SoyBean" Then
+            If Days >= 45 Or AvgHeight >= 24 Then
+                If AvgHeight >= 12 Then
+                    Return True
+                End If
+            End If
+        ElseIf strType = "Wheat" Then
+        End If
+        Return False
+    End Function
+
+
+End Class
--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -23,48 +23,18 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.TabControl1 = New System.Windows.Forms.TabControl()
-        Me.TabPage1 = New System.Windows.Forms.TabPage()
-        Me.TabPage2 = New System.Windows.Forms.TabPage()
         Me.TabPage3 = New System.Windows.Forms.TabPage()
-        Me.TabPage4 = New System.Windows.Forms.TabPage()
-        Me.TabPage5 = New System.Windows.Forms.TabPage()
-        Me.TabPage6 = New System.Windows.Forms.TabPage()
         Me.TabControl1.SuspendLayout()
         Me.SuspendLayout()
         '
         'TabControl1
         '
-        Me.TabControl1.Controls.Add(Me.TabPage1)
-        Me.TabControl1.Controls.Add(Me.TabPage2)
         Me.TabControl1.Controls.Add(Me.TabPage3)
-        Me.TabControl1.Controls.Add(Me.TabPage4)
-        Me.TabControl1.Controls.Add(Me.TabPage5)
-        Me.TabControl1.Controls.Add(Me.TabPage6)
         Me.TabControl1.Location = New System.Drawing.Point(12, 12)
         Me.TabControl1.Name = "TabControl1"
         Me.TabControl1.SelectedIndex = 0
         Me.TabControl1.Size = New System.Drawing.Size(1045, 780)
         Me.TabControl1.TabIndex = 0
-        '
-        'TabPage1
-        '
-        Me.TabPage1.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage1.Name = "TabPage1"
-        Me.TabPage1.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage1.Size = New System.Drawing.Size(1037, 754)
-        Me.TabPage1.TabIndex = 0
-        Me.TabPage1.Text = "TabPage1"
-        Me.TabPage1.UseVisualStyleBackColor = True
-        '
-        'TabPage2
-        '
-        Me.TabPage2.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage2.Name = "TabPage2"
-        Me.TabPage2.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage2.Size = New System.Drawing.Size(1037, 754)
-        Me.TabPage2.TabIndex = 1
-        Me.TabPage2.Text = "TabPage2"
-        Me.TabPage2.UseVisualStyleBackColor = True
         '
         'TabPage3
         '
@@ -75,36 +45,6 @@ Partial Class Form1
         Me.TabPage3.TabIndex = 2
         Me.TabPage3.Text = "TabPage3"
         Me.TabPage3.UseVisualStyleBackColor = True
-        '
-        'TabPage4
-        '
-        Me.TabPage4.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage4.Name = "TabPage4"
-        Me.TabPage4.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage4.Size = New System.Drawing.Size(1037, 754)
-        Me.TabPage4.TabIndex = 3
-        Me.TabPage4.Text = "TabPage4"
-        Me.TabPage4.UseVisualStyleBackColor = True
-        '
-        'TabPage5
-        '
-        Me.TabPage5.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage5.Name = "TabPage5"
-        Me.TabPage5.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage5.Size = New System.Drawing.Size(1037, 754)
-        Me.TabPage5.TabIndex = 4
-        Me.TabPage5.Text = "TabPage5"
-        Me.TabPage5.UseVisualStyleBackColor = True
-        '
-        'TabPage6
-        '
-        Me.TabPage6.Location = New System.Drawing.Point(4, 22)
-        Me.TabPage6.Name = "TabPage6"
-        Me.TabPage6.Padding = New System.Windows.Forms.Padding(3)
-        Me.TabPage6.Size = New System.Drawing.Size(1037, 754)
-        Me.TabPage6.TabIndex = 5
-        Me.TabPage6.Text = "TabPage6"
-        Me.TabPage6.UseVisualStyleBackColor = True
         '
         'Form1
         '
@@ -120,10 +60,5 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents TabControl1 As TabControl
-    Friend WithEvents TabPage1 As TabPage
-    Friend WithEvents TabPage2 As TabPage
     Friend WithEvents TabPage3 As TabPage
-    Friend WithEvents TabPage4 As TabPage
-    Friend WithEvents TabPage5 As TabPage
-    Friend WithEvents TabPage6 As TabPage
 End Class
